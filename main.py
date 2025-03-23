bdfe65fbc068fe0cce34dc5563c3e9b757b4a035
Nifemi Oyekunle <oyek0004@algonquinlive.com>
2025-03-22 21:32:02 -0400

    added blacklisting of used JWTs during logout

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template, session, request, redirect, url_for, jsonify
 from database import db
 from models import User, bcrypt
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required, get_jwt
+from datetime import timedelta
 
 app = Flask(__name__) #creates a Flask app
-app.secret_key = "neo_from_the_matrix" 
 
-#we have to configure the database
+#CONFIGURATIONS
 
 # database configuration
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db" #sets the database URI to the users.db file
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False #False so it does not track changes in the database
+
+#key configurations
+app.config["SECRET_KEY"] = "neo_from_the_matrix_secret_key" # this needs to be a secure random key
+app.config["JWT_SECRET_KEY"] = "neo_from_the_matrix_JWT_key" # this needs to be a secure random key
+
+#JWT configurations
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=30) #sets the time for the access token to expire
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=7) #sets the time for the refresh token to expire
 
 # initializing database
 db.init_app(app) # inits the database based on Flask app name
@@ -17,7 +26,10 @@ db.init_app(app) # inits the database based on Flask app name
 #initialise bcrypt (password hasher)
 bcrypt.init_app(app)
 
-# routing (routes)
+#initialise JWT
+jwt = JWTManager(app)
+
+# ROUTING (routes)
 
 #home route
 @app.route("/") # sets the route 
@@ -38,14 +50,17 @@ def login():
 #checks database for if info exists
   existing_user = User.query.filter_by(email=email).first() 
   if existing_user and existing_user.check_password(password): 
-    #redirects to dashboard if info exists
+
+    access_token = create_access_token(identity=existing_user.id) #creates access token for user if user exists
+    refresh_token =create_access_token(identity=existing_user.id, fresh=False) #false because it is not a new token
     session["email"] = email
-    return jsonify({"success": "Login successful"}), 200
-    # return redirect(url_for("dashboard")) #!!!
+    
+    return jsonify({"access_token": access_token, "refresh_token": refresh_token}), 200
+    # return redirect(url_for("dashboard")) #redirects to dashboard if info exists #!!!    
   else:
     #redirects to register page if info does not exist
-    return render_template("register.html", error="Account does not exist")
-    # return jsonify({"error": "Account does not exist"}), 400 #!!!
+    return jsonify({"error": "Account does not exist"}), 400 
+    # return render_template("register.html", error="Account does not exist") #!!!
 
 
 # register route
@@ -57,30 +72,62 @@ def register():
   email = data["email"]
   password = data["password"]
   
-  # checks if user already exists
   existing_user = User.query.filter_by(email=email).first()
-  if existing_user:
+  
+  if existing_user: # checks if user already exists
     return jsonify({"error": "Account already exists"}), 400
     # return render_template("index.html", error="Account already exists") #!!!
   else:
-    #if user isnt already in db create new user
-    new_user = User(email=email, username=username)
-    #sets password
-    new_user.set_password(password)
-    
-    #stores user in database
-    db.session.add(new_user) 
+    new_user = User(email=email, username=username) #if user isnt already in db create new user
+    new_user.set_password(password) #sets password
+  
+    db.session.add(new_user) #stores user in database
     db.session.commit() #commits changes to database
     
     #opens a session and redirects to dashboard
     session["email"] = email 
     # return redirect(url_for("dashboard")) #!!!
     return jsonify({"success": "Account created"}), 200 
+  
+# protected route
+@app.route("/protected")
+def protected():
+  
+  user_id = get_jwt_identity() #gets user id of person trying to access protected route
+  user = User.query.filter_by(id=user_id).first() #gets user from database
+  
+  if user:
+    return jsonify({"email": user.email}), 200 #if user exists returns email
+  else:
+    return jsonify({"error: User does not exist"}), 400
+  
 
+# refresh route
+@app.route("/refresh", methods=["POST"])
+@jwt_required(refresh=True) #requires a refresh token to access this route
+def refresh():
+  existing_user_id = get_jwt_identity() #gets user id of person trying to access refresh route
+  access_token = create_access_token(identity=existing_user_id) #creates a new access token for the user
+  return jsonify({"access_token": access_token}), 200
 
 # dashboard route
 
 # logout route
+@app.route("/logout")
+@jwt_required() #requires a jwt token to access this route
+def logout():
+  jti = get_jwt()["jti"] #gets the jti from the jwt token
+  token_blacklist.add(jti) #adds the jti to the token blacklist
+  return jsonify({"message": "Successfully logged out"}), 200
+
+#blacklist token code
+token_blacklist = set()
+
+@jwt.token_in_blacklist_loader # indicates that JWT library will load and check if token is blacklisted
+def check_if_token_is_blacklisted(jwt_header, jwt_data):
+  jti = jwt_data["jti"] #gets the jti(unique identifier) from the jwt data
+  return jti in token_blacklist #checks if the jti is in the token blacklist
+
 
 if __name__ == "__main__": #runs the app if this file is the main file
   with app.app_context(): #ensures that the database is created while knowing which app configuration to use
